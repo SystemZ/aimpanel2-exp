2627c89a7fd27d98091162109de98e906041aa05
Kacper Sawicki <kacper@lvlup.pro>
2018-09-25 14:24:58 +0200

    Every new log line is published to rabbitmq queue

--- fake-server.sh
+++ fake-server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-while true; do echo "smth"; sleep 5; done & while read str; do echo "Catched: $str"; done;
+while true; do echo "smth"; sleep 5; done & while read str; do echo "Catched:$str"; done;
--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"github.com/streadway/amqp"
 	"io"
 	"log"
 	"os/exec"
 	"time"
 )
 
+var (
+	ch *amqp.Channel
+	q  amqp.Queue
+
+	stdin  io.WriteCloser
+	stdout io.ReadCloser
+)
+
 func main() {
+	conn, err := amqp.Dial("amqp://admin:admin@46.105.209.74:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err = conn.Channel()
+	failOnError(err, "Failed to open channel")
+	defer ch.Close()
+
+	q, err = ch.QueueDeclare("wrapper", false, false, false, false, nil)
+	failOnError(err, "Failed to declare a queue")
+
+	startServer()
+}
+
+func startServer() {
 	cmd := exec.Command("bash", "fake-server.sh")
 
-	stdout, _ := cmd.StdoutPipe()
-	stdin, _ := cmd.StdinPipe()
+	stdout, _ = cmd.StdoutPipe()
+	stdin, _ = cmd.StdinPipe()
 
 	if err := cmd.Start(); err != nil {
 		log.Fatalln(err)
@@ -25,13 +50,26 @@ func main() {
 		}
 	}()
 
+	readStdout()
+
+	if err := cmd.Wait(); err != nil {
+		log.Println(err)
+	}
+}
+
+func readStdout() {
 	scanner := bufio.NewScanner(stdout)
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
 		log.Println(scanner.Text())
+		err := ch.Publish("", q.Name, false, false, amqp.Publishing{ContentType: "text/plain", Body: []byte(scanner.Text())})
+		failOnError(err, "Publish error")
 	}
+}
 
-	if err := cmd.Wait(); err != nil {
-		log.Println(err)
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
