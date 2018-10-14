836744354f55503846a1d6e004e3d6d970c4a6a2
Kacper Sawicki <kacper@lvlup.pro>
2018-10-14 20:22:48 +0200

    Added pages and prepared nav and toolbar layout. Added data table in hosts page #30

--- master-frontend/src/App.vue
+++ master-frontend/src/App.vue
@@ -1,92 +1,79 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
+    <v-app>
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                dark
+                absolute
         >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'fas fa-chevron-right' : 'fas fa-chevron-left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>fas fa-globe</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>fas fa-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>fas fa-bars</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>fas fa-exchange-alt</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+            <v-toolbar flat>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title class="title">
+                            <p class="text-xs-center font-weight-regular font-weight-thin">Aimpanel</p>
+                        </v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-list class="pt-0" dense>
+                <v-divider></v-divider>
+                <v-list-tile v-for="item in menu"
+                             :to="item.path"
+                             active-class="red--text red--darken-1">
+                    <v-list-tile-action>
+                        <v-icon>{{item.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{item.title}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="red darken-1" dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>{{$route.meta.title}}</v-toolbar-title>
+        </v-toolbar>
+        <v-content>
+            <router-view/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'fas fa-shapes',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+  export default {
+    name: 'Aimpanel',
+    data: () => ({
+      drawer: null,
+      menu: [
+        {
+          title: 'Home',
+          icon: 'fa-home',
+          path: '/'
+        },
+        {
+          title: 'Hosts',
+          icon: 'fa-server',
+          path: '/hosts'
+        },
+        {
+          title: 'Game servers',
+          icon: 'fa-gamepad',
+          path: '/game-servers'
+        },
+        {
+          title: 'License',
+          icon: 'fa-certificate',
+          path: '/license'
+        },
+        {
+          title: 'Help',
+          icon: 'fa-question',
+          path: '/help'
+        }
+      ]
+    }),
+    mounted() {
+      console.log(this.$route)
     }
   }
-}
 </script>
--- master-frontend/src/router.js
+++ master-frontend/src/router.js
@@ -11,15 +11,45 @@ export default new Router({
     {
       path: '/',
       name: 'home',
+      meta: {
+        title: 'Home'
+      },
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/hosts',
+      name: 'hosts',
+      meta: {
+        title: 'Hosts'
+      },
+      component: () => import(/* webpackChunkName: "hosts" */ './views/Hosts.vue')
+    },
+    {
+      path: '/game-servers',
+      name: 'game-servers',
+      meta: {
+        title: 'Game Servers'
+      },
+      component: () => import(/* webpackChunkName: "game-servers" */ './views/GameServers.vue')
+    },
+    {
+      path: '/license',
+      name: 'license',
+      meta: {
+        title: 'License'
+      },
+      component: () => import(/* webpackChunkName: "license" */ './views/License.vue')
+    },
+    {
+      path: '/help',
+      name: 'help',
+      meta: {
+        title: 'Help'
+      },
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "help" */ './views/Help.vue')
     }
   ]
 })
