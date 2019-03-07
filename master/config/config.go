eb409503d240e23acb5f6eb9ae24e4f0ee55323d
Kacper Sawicki <kacper@lvlup.pro>
2019-03-07 20:48:55 +0100

    Fix for master config

--- master/config/config.go
+++ master/config/config.go
@@ -15,12 +15,12 @@ var (
 func init() {
 	viper.SetDefault("DB_HOST", "localhost")
 	DB_HOST = viper.GetString("DB_HOST")
-	viper.SetDefault("DB_PORT", "5432")
+	viper.SetDefault("DB_PORT", "3306")
 	DB_PORT = viper.GetString("DB_PORT")
 	viper.SetDefault("DB_USERNAME", "dev")
 	DB_USERNAME = viper.GetString("DB_USERNAME")
 	viper.SetDefault("DB_PASSWORD", "dev")
 	DB_PASSWORD = viper.GetString("DB_PASSWORD")
 	viper.SetDefault("DB_NAME", "dev")
-	DB_PASSWORD = viper.GetString("DB_NAME")
+	DB_NAME = viper.GetString("DB_NAME")
 }
