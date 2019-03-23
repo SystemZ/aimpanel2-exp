8642a15dc13b113299bebb31f40064d62454446a
Kacper Sawicki <kacper@lvlup.pro>
2019-03-23 19:56:31 +0100

    Added created,deleted,updated at to game command table

--- master/model/game_command.go
+++ master/model/game_command.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type GameCommand struct {
 	ID uint `gorm:"primary_key" json:"id"`
 
@@ -12,4 +14,13 @@ type GameCommand struct {
 	Order uint `gorm:"column:order" json:"order"`
 
 	Command string `gorm:"column:command" json:"command"`
+
+	//Created at timestamp
+	CreatedAt time.Time `json:"created_at"`
+
+	//Updated at timestamp
+	UpdatedAt time.Time `json:"updated_at"`
+
+	//Deleted at timestamp
+	DeletedAt *time.Time `json:"deleted_at"`
 }
