1827eb1ee8fe8dc8d362b90369d0e3ed404f69ef
SystemZ <hello@systemz.pl>
2019-12-22 20:05:09 +0100

    Upload slave binary to bucket

--- Taskfile.sh
+++ Taskfile.sh
@@ -32,6 +32,12 @@ function update-todo-issue {
     curl -X PUT -H "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" -F "description=$ISSUE_CONTENT" https://gitlab.com/api/v4/projects/7001381/issues/157
 }
 
+function upload-slave-binary {
+    echo "$RCLONE_CONF" > rclone.conf
+    rclone -v --stats-one-line-date --config rclone.conf copyto aimpanel-slave ovh-bucket:aimpanel-updates/$CI_COMMIT_SHA
+    curl -XPOST -H "Token: $AIMPANEL_UPDATE_TOKEN" -H "Content-type: application/json" -d "{\"commit\": \"$CI_COMMIT_SHA\", \"url\": \"https://storage.gra.cloud.ovh.net/v1/AUTH_23b9e96be2fc431d93deedba1b8c87d2/aimpanel-updates/$CI_COMMIT_SHA\"" 'http://my-lab.aimpanel.pro/v1/version'
+}
+
 function default {
     help
 }
