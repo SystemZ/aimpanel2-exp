6b7581e6f12fef02b4760a703ad765b8a345ea17
Kacper Sawicki <kacper@lvlup.pro>
2019-12-22 19:08:46 +0100

    Added version to master and slave

--- slave/cmd/dev.go
+++ slave/cmd/dev.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"github.com/r3labs/sse"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -16,17 +14,5 @@ var devCmd = &cobra.Command{
 	Long:  "",
 	//Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		//lib.GetGameCmd()
-		client := sse.NewClient("http://localhost:9000/v1/events/x")
-		client.Headers = map[string]string{
-			"Authorization": "Bearer x",
-		}
-		err := client.SubscribeRaw(func(msg *sse.Event) {
-			logrus.Info(string(msg.ID))
-			logrus.Info(string(msg.Data))
-		})
-		if err != nil {
-			logrus.Info(err)
-		}
 	},
 }
