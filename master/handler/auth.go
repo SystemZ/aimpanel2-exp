50ac531ce565c094d535ee3c2f678aea82ef1d0f
Kacper Sawicki <kacper@lvlup.pro>
2019-12-27 20:20:49 +0100

    Removed swagger comments from auth

--- master/handler/auth.go
+++ master/handler/auth.go
@@ -4,53 +4,23 @@ import (
 	"encoding/json"
 	"gitlab.com/systemz/aimpanel2/lib"
 	"gitlab.com/systemz/aimpanel2/lib/ecode"
+	"gitlab.com/systemz/aimpanel2/lib/response"
 	"gitlab.com/systemz/aimpanel2/master/model"
 	"net/http"
 )
 
-//swagger:model registerRequest
 type AuthRegisterRequest struct {
-	// required: true
-	Username string `json:"username"`
-	// required: true
-	Password string `json:"password"`
-	// required: true
+	Username       string `json:"username"`
+	Password       string `json:"password"`
 	PasswordRepeat string `json:"password_repeat"`
-	// required: true
-	Email string `json:"email"`
-	// required: true
-	EmailRepeat string `json:"email_repeat"`
+	Email          string `json:"email"`
+	EmailRepeat    string `json:"email_repeat"`
 }
 
-//swagger:parameters Auth Login
 type AuthLoginRequest struct {
-	// User name
-	//
-	// in: body
-	// required: true
 	Username string `json:"username"`
-
-	// User password
-	//
-	// in: body
-	// required: true
 	Password string `json:"password"`
 }
-
-//swagger:model Token
-type Token struct {
-	// JWT token
-	Token string `json:"token"`
-}
-
-// swagger:route POST /auth/register Auth Register
-//
-// Create new account
-//
-//Responses:
-//	default: jsonError
-//  400: jsonError
-//	200: Token
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
@@ -128,17 +98,8 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	model.DB.Save(groupUser)
 	model.CreatePermissionsForNewUser(group.ID)
 
-	lib.MustEncode(json.NewEncoder(w), Token{Token: token})
+	lib.MustEncode(json.NewEncoder(w), response.Token{Token: token})
 }
-
-// swagger:route POST /auth/login Auth Login
-//
-// Get session token
-//
-//Responses:
-//	default: jsonError
-//  400: jsonError
-//	200: Token
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
@@ -166,7 +127,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		lib.MustEncode(json.NewEncoder(w), Token{Token: token})
+		lib.MustEncode(json.NewEncoder(w), response.Token{Token: token})
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
 
