165dc6210fb548043dfe665521c181c835ef6751
Kacper Sawicki <kacper@lvlup.pro>
2020-01-26 10:48:55 +0100

    Moved logic from user handlers

--- lib/request/request.go
+++ lib/request/request.go
@@ -26,3 +26,17 @@ type GameServerStop struct {
 type GameServerSendCommand struct {
 	Command string `json:"command" example:"say Hello!"`
 }
+
+//swagger:parameters User changePassword
+type UserChangePasswordReq struct {
+	Password          string `json:"password"`
+	NewPassword       string `json:"new_password"`
+	NewPasswordRepeat string `json:"new_password_repeat"`
+}
+
+//swagger:parameters User changeEmail
+type UserChangeEmailReq struct {
+	Email          string `json:"email"`
+	NewEmail       string `json:"new_email"`
+	NewEmailRepeat string `json:"new_email_repeat"`
+}
---
+++ master/service/user/user.go
@@ -0,0 +1,37 @@
+package user
+
+import (
+	"gitlab.com/systemz/aimpanel2/lib/ecode"
+	"gitlab.com/systemz/aimpanel2/lib/request"
+	"gitlab.com/systemz/aimpanel2/master/model"
+)
+
+func ChangePassword(data *request.UserChangePasswordReq, user *model.User) int {
+	if user.CheckPassword(data.Password) {
+		if data.NewPassword == data.NewPasswordRepeat {
+			user.PasswordHash = user.HashPassword(data.NewPassword)
+			model.DB.Save(&user)
+
+			return ecode.NoError
+		} else {
+			return ecode.PasswordsDoNotMatch
+		}
+	} else {
+		return ecode.WrongPassword
+	}
+}
+
+func ChangeEmail(data *request.UserChangeEmailReq, user *model.User) int {
+	if user.Email == data.Email {
+		if data.NewEmail == data.NewEmailRepeat {
+			user.Email = data.NewEmail
+			model.DB.Save(&user)
+
+			return ecode.NoError
+		} else {
+			return ecode.EmailsDoNotMatch
+		}
+	} else {
+		return ecode.WrongEmail
+	}
+}
