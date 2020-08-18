4360bcd25bd4f72414a1e8fb2074cd5fa6c903b9
SystemZ <hello@systemz.pl>
2020-08-18 15:09:12 +0200

    Use host token from ENV in agent, unambiguous source of token
    Host token passed to Start method can be used later in testing

--- slave/cmd/agent.go
+++ slave/cmd/agent.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"gitlab.com/systemz/aimpanel2/slave/agent"
+	"gitlab.com/systemz/aimpanel2/slave/config"
 )
 
 func init() {
@@ -10,11 +11,10 @@ func init() {
 }
 
 var agentCmd = &cobra.Command{
-	Use:   "agent [token]",
+	Use:   "agent",
 	Short: "Start agent",
 	Long:  "",
-	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		agent.Start(args[0])
+		agent.Start(config.HOST_TOKEN)
 	},
 }
