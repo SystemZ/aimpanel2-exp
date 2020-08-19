7313539d4a5860f65c217f5ff55aafd28eefb20a
SystemZ <hello@systemz.pl>
2020-08-19 21:33:19 +0200

    Hardcoded game versions for simplicity in early stage of project

--- master/model/game_file.go
+++ master/model/game_file.go
@@ -3,8 +3,10 @@ package model
 import (
 	"context"
 	"fmt"
+	"gitlab.com/systemz/aimpanel2/lib/game"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"strconv"
 )
 
 type GameFile struct {
@@ -29,7 +31,7 @@ func (g *GameFile) SetID(id primitive.ObjectID) {
 	g.ID = id
 }
 
-func GetGameFileByGameIdAndVersion(gameId uint, version string) (*GameFile, error) {
+func GetGameFileByGameIdAndVersionFromMongo(gameId uint, version string) (*GameFile, error) {
 	var gf GameFile
 
 	err := DB.Collection(gameFileCollection).FindOne(context.TODO(), bson.D{
@@ -44,4 +46,32 @@ func GetGameFileByGameIdAndVersion(gameId uint, version string) (*GameFile, erro
 	}
 
 	return &gf, nil
+}
+
+func GetGameFileByGameIdAndVersion(gameId uint, version string) (gameFile *GameFile, err error) {
+	downloadUrl := ""
+	switch gameId {
+	case game.GAME_BUNGEECORD:
+		downloadUrl = fmt.Sprintf("https://ci.md-5.net/job/BungeeCord/%s/artifact/bootstrap/target/BungeeCord.jar", version)
+	case game.GAME_SPIGOT:
+		downloadUrl = "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+	case game.GAME_TEAMSPEAK3:
+		downloadUrl = fmt.Sprintf("https://files.teamspeak-services.com/releases/server/%s/teamspeak3-server_linux_amd64-%s.tar.bz2", version, version)
+	case game.GAME_TS3AUDIOBOT:
+		downloadUrl = fmt.Sprintf("https://splamy.de/api/nightly/ts3ab/%s/download", version)
+	case game.GAME_MC_VANILLA:
+		switch version {
+		case "1.16.2":
+			downloadUrl = "https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar"
+		}
+		// bedrock
+		// https://www.minecraft.net/en-us/download/server/bedrock
+	}
+
+	gameFile = &GameFile{
+		GameId:      strconv.Itoa(int(gameId)),
+		GameVersion: version,
+		DownloadUrl: downloadUrl,
+	}
+	return
 }
