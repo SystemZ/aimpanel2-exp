46e2072d5c4805054948b769961ec0897099f5cc
Kacper Sawicki <kacper@ksawicki.pl>
2020-11-25 21:14:52 +0100

    Add dev-slave-cert command to add & generate self signed cert for slave

--- master/service/cert/cert.go
+++ master/service/cert/cert.go
@@ -1,12 +1,17 @@
 package cert
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"github.com/go-acme/lego/v4/certcrypto"
@@ -17,6 +22,10 @@ import (
 	"gitlab.com/systemz/aimpanel2/master/config"
 	"gitlab.com/systemz/aimpanel2/master/model"
 	"io"
+	"math/big"
+	"net"
+	"os"
+	"time"
 )
 
 var (
@@ -157,4 +166,84 @@ func CreateCerts() error {
 	}
 
 	return nil
+}
+
+//https://gist.github.com/samuel/8b500ddd3f6118d052b5e6bc16bc4c09
+func CreateSelfSignedCert(domain model.CertDomain) error {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Aimpanel"},
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	if ip := net.ParseIP(domain.Name); ip != nil {
+		template.IPAddresses = append(template.IPAddresses, ip)
+	} else {
+		template.DNSNames = append(template.DNSNames, domain.Name)
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(privateKey), privateKey)
+	if err != nil {
+		return err
+	}
+
+	dbCert := &model.Cert{
+		DomainId: domain.ID,
+	}
+
+	out := &bytes.Buffer{}
+
+	pem.Encode(out, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	dbCert.Cert = out.String()
+
+	out.Reset()
+
+	pem.Encode(out, pemBlockForKey(privateKey))
+	dbCert.PrivateKey = out.String()
+
+	err = model.Put(dbCert)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func publicKey(priv interface{}) interface{} {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &k.PublicKey
+	case *ecdsa.PrivateKey:
+		return &k.PublicKey
+	default:
+		return nil
+	}
+}
+
+func pemBlockForKey(priv interface{}) *pem.Block {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k)}
+	case *ecdsa.PrivateKey:
+		b, err := x509.MarshalECPrivateKey(k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to marshal ECDSA private key: %v", err)
+			os.Exit(2)
+		}
+		return &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+	default:
+		return nil
+	}
 }
