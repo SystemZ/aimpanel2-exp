286adf61a14819090dcada1e110ee72a7e9492f3
Michał Frąckiewicz <hello@systemz.pl>
2022-06-18 19:02:53 +0200

    Helper for system testing external URLs

--- cabbage/test/application_system_test_case.rb
+++ cabbage/test/application_system_test_case.rb
@@ -3,4 +3,12 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
   # using: :chrome
+
+  # https://stackoverflow.com/questions/45483353/cannot-remove-port-from-capybara-while-visiting-external-link
+  def visit_external(url)
+    Capybara.always_include_port = false
+    visit url
+    Capybara.always_include_port = true
+  end
+
 end
