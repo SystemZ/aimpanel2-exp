93eb7666b7dea4ef2f752af8541963177e7c500e
Michał Frąckiewicz <hello@systemz.pl>
2022-06-19 20:27:04 +0200

    Routing for multiple panel domains

--- cabbage/config/routes.rb
+++ cabbage/config/routes.rb
@@ -5,12 +5,18 @@ Rails.application.routes.draw do
   # https://guides.rubyonrails.org/routing.html#advanced-constraints
   # https://blog.appsignal.com/2020/03/04/building-a-rails-app-with-multiple-subdomains.html
   # https://stackoverflow.com/questions/6054668/rails-3-routing-and-multiple-domains/6058737#6058737
-  constraints domain: "lvlup.pro" do
-    devise_for :users
-    resources :websites
-    resources :domains
-    resources :pages
-  end
+
+  # TODO use config / env for domains
+  %w[lvlup.pro localhost].each { |domain|
+    constraints domain: domain do
+      devise_for :users, as: "devise_"+domain.gsub(".","_")
+      resources :websites
+      resources :domains
+      resources :pages
+      match '/p/:website/' => 'pages#page_by_slug', via: :get, as: "match_website_root_"+domain.gsub(".","_")
+      match '/p/:website/*path' => 'pages#page_by_slug', via: :get, as: "match_website_path_"+domain.gsub(".","_")
+    end
+  }
 
   root :to => "pages#page_by_slug_and_domain"
   match '*path' => 'pages#page_by_slug_and_domain', via: :get
