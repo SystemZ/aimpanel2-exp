d9efa62e5b16e5b006b1157a84a8dac92701f95f
Paulina Jabłońska <hello@aylin.dev>
2022-09-13 14:08:05 +0200

    Add desktop and mobile navbars for server list page

---
+++ templates/index.js
@@ -0,0 +1,11 @@
+// mobile menu
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', (e) => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+});
